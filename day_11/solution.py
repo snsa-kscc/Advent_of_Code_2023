9d57f494f15cac559029c143648c18ebfe4f6692
Sinisa Koscec <sinisa@dvasadva.com>
2023-12-11 16:47:26 +0100

    day eleven

---
+++ day_11/solution.py
@@ -0,0 +1,19 @@
+with open("day_11/data.txt") as f:
+  data = f.read().splitlines()
+
+empty_rows = [r for r, row in enumerate(data) if all(ch == "." for ch in row)]
+empty_cols = [c for c, col in enumerate(zip(*data)) if all(ch == "." for ch in col)]
+
+points = [(r, c) for r, row in enumerate(data) for c, ch in enumerate(row) if ch == "#"]
+
+total = 0
+scale = 2 # 1000000 for part 2
+
+for i, (r1, c1) in enumerate(points):
+  for (r2, c2) in points[:i]:
+    for r in range(min(r1, r2), max(r1, r2)):
+      total += scale if r in empty_rows else 1
+    for c in range(min(c1, c2), max(c1, c2)):
+      total += scale if c in empty_cols else 1
+
+print(total)
